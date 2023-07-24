84c5cc8d5faa41055b372c0f4a439d4d698c1148
roy <roy@ubuntu.myguest.virtualbox.org>
2023-07-24 22:47:05 +0100

    Implement reference checker

---
+++ reference_checker.py
@@ -0,0 +1,121 @@
+from scholarly import scholarly
+import re
+# From Scriber
+# Harvard style: 
+#   Books: Author surname, initial. (Year) Book title. City: Publisher.
+#   journal: Author surname, initial. (Year) ‘Article title’, Journal Name, Volume(Issue), pp. page range.
+
+# APA style: 
+#   Books: Author’s Last Name, Initial(s). (Year of publication). Title of book. Publisher.
+#   journal: Author’s Last Name, Initial(s). (Year of publication). Title of article. Title of Journal, Volume(Issue), Pages.
+
+# MLA style: 
+#   Books:  Author’s Last Name, First Name. Title of Book. Publisher, Year of publication.
+#   journal: Author’s Last Name, First Name. “Title of Article.” Title of Journal, vol. Volume, no. Issue, Month Year of publication, pp. Pages.
+
+def check_paper_exists(citation) -> str:
+    # Search for publications with the given title
+    title = extract_title(citation)
+    search_query = scholarly.search_pubs(title)
+
+    try:
+        # Get the first publication from the search results
+        pub = next(search_query)
+        return pub['bib']['title'].lower()
+      
+    except StopIteration:
+        # If no search results were found, the paper does not exist
+        return "NA"
+
+def apa_citation(citation):
+    author =  r'[A-Z][a-z]+\,(\s[A-Z]\.)+(\,\s[A-Z][a-z]+\,\s([A-Z]\.)+)*\,\s\&(\s[A-Z][a-z]+\,\s[A-Z]\.)?'
+    year   = r'\s\(\d{4}\)\.'
+    main_title  = r'(\s(\w+(\s\w+)+)\:)?'
+    sub_title = r'\s(\w+(\s\w+)+)\.'
+    publisher = r'\s(\w+(\s\w+)+)\,'
+    vol_page = r'\s+\d+\(\d+\)\,\s+\d+(-|–)\d+'
+    doi = r""
+
+
+    apa_pattern_without_doi = author + year + main_title + sub_title + publisher + vol_page
+    if re.match(apa_pattern_without_doi, citation):
+        return True
+    else:
+        return False
+    
+def harvard_citation(citation):
+    # only single author supported right now 
+    author =  r'[A-Z][a-z]+\,(\s[A-Z]\.)+'
+    year   = r'\s\(\d{4}\)'
+    title = r'\s(\w+(\s\w+)+)\.'
+    publisher = r'\s(\w+(\s\w+)+)\,'
+    vol_page = r'\s+\d+\(\d+\)\,\spp\.\s+\d+(-|–)\d+'
+    
+def extract_title(citation):
+    if 'pp.' in citation or 'vol.' in citation:
+        return extract_journal_title(citation)
+    else:
+        return extract_book_title(citation)
+    
+
+def extract_book_title(citation):
+    # find the year of publication
+    year_match = re.search(r'\((\d{4})\)', citation)
+    
+    # check if it's followed by the title (Harvard or APA style).
+    if year_match:
+        year_index = year_match.end()
+        # Extract the title
+        title_match = re.search(r'\.\s(.*?)\.\s', citation[year_index:])
+        
+        if title_match:
+            return title_match.group(1)
+    
+    # If the year is not found then MLA style.
+    title_match = re.search(r'\.\s(.*?)\.\s', citation)
+    
+    if title_match:
+        return title_match.group(1)
+    
+    # error message if title is not found.
+    return "Title not found"
+
+def extract_journal_title(citation):
+    # find the year of publication
+    year_match = re.search(r'\((\d{4})\)', citation)
+
+    # check if it's followed by the title (Harvard or APA style).
+    if year_match:
+        year_index = year_match.end()
+        # Extract the title
+        title_match = re.search(r'\.\s(.*?)\,\s', citation[year_index:])
+        
+        if title_match:
+            return title_match.group(1)
+    
+    # If the year is not found assume MLA style.
+    title_match = re.search(r'(\"|“)(.*?)(\"|”)', citation)
+    
+    # If the title is found, return it.
+    if title_match:
+        return title_match.group(2)
+    
+    # error message if title is not found.
+    return "Title not found"
+
+
+
+# Test the function with an example citation.
+citation = "Andreff, Wladimir, and Paul D. Staudohar. “The Evolving European Model of Professional Sports Finance.” Journal of Sports Economics, vol. 1, no. 3, Sept. 2000, pp. 257–76, https://doi.org/10.1177/152700250000100304."
+print(extract_journal_title(citation))
+
+
+# Test the function with an example citation.
+citation = "Smith, Thomas, and Barbara Michelle Williams. The Citation Manual for Students: A Quick Guide. Wiley, 2020."
+print(extract_book_title(citation))
+
+print(check_paper_exists(citation))
+
+
+
+
---
+++ test2.py
@@ -0,0 +1,91 @@
+import re
+
+# def is_apa_citation(citation):
+#     author =  r'[A-Z][a-z]+\,(\s[A-Z]\.)+(\,\s[A-Z][a-z]+\,\s([A-Z]\.)+)*\,\s\&(\s[A-Z][a-z]+\,\s[A-Z]\.)?'
+#     year   = r'\s\(\d{4}\)\.'
+#     main_title  = r'(\s(\w+(\s\w+)+)\:)?'
+#     sub_title = r'\s(\w+(\s\w+)+)\.'
+#     publisher = r'\s(\w+(\s\w+)+)\,'
+#     extra = r'\s+\d+\(\d+\)\,\s+\d+(-|–)\d+'
+#     doi = r""
+
+
+#     apa_pattern_without_doi = author + year + main_title + sub_title + publisher + extra
+#     if re.match(apa_pattern_without_doi, citation):
+#         return True
+#     else:
+#         return False
+
+
+
+
+# # Test the function
+# citation = "Doe, J., & Smith, J. (2023). An analysis of AI advancements. Journal of Technology Studies, 20(4), 350-375. https://doi.org/10.1234/jts2023.104"
+# print(is_apa_citation(citation))  # Should print: True
+
+# citation = "Doe, J., & Smith, J. (2023). An analysis of AI advancements. Journal of Technology Studies, 20(4), 350-375."
+# print(is_apa_citation(citation))  # Should print: True
+
+# citation = "Not an APA citation"
+# print(is_apa_citation(citation))  # Should print: False
+
+# citation = "Grady, J. S., Her, M., Moreno, G., Perez, C., & Yelinek, J. (2019). Emotions in storybooks: A comparison of storybooks that represent ethnic and racial groups in the United States. Psychology of Popular Media Culture, 8(3), 207–217. https://doi.org/10.1037/ppm0000185"
+# print(is_apa_citation(citation))  # Should print: True
+
+
+
+
+
+# author =  r'[A-Z][a-z]+\,(\s[A-Z]\.)+(\,\s[A-Z][a-z]+\,\s([A-Z]\.)+)*\,\s\&(\s[A-Z][a-z]+\,\s[A-Z]\.)?'
+# year   = r'\s\(\d{4}\)\.'
+# main_title  = r'(\s([A-Za-z]+(\s[A-Za-z]+)+)\:)?'
+# sub_title = r'\s([A-Za-z]+(\s[A-Za-z]+)+)\.'
+# publisher = r'\s([A-Za-z]+(\s[A-Za-z]+)+)\,'
+# extra = r'\s+\d+\(\d+\)\,\s+\d+(-|–)\d+'
+# pp = r"pp." 
+# doi = r""
+# cit = "Doe, J., & Smith, J. (2023). An analysis of AI advancements. Journal of Technology Studies, 20(4), 350375."
+# print(re.match(author + year + main_title + sub_title + publisher + extra, cit))
+
+
+
+# vol_page = r'\s+\d+\(\d+\)\,\spp\.\s+\d+(-|–)\d+'
+# print(re.match(vol_page, " 5(2), pp. 121–139."))
+
+
+import re
+
+def extract_title(citation):
+    # First, find the year of publication by searching for four digits within parentheses.
+    year_match = re.search(r'\((\d{4})\)', citation)
+    
+    # If the year is found, check if it's followed by the title (Harvard or APA style).
+    if year_match:
+        year_index = year_match.end()
+        # Extract the title by finding the text between the year and the next period.
+        title_match = re.search(r'\.\s(.*?)\.\s', citation[year_index:])
+        
+        # If the title is found, return it.
+        if title_match:
+            return title_match.group(1)
+    
+    # If the year is not found or the title is not found after the year, assume MLA style.
+    # Extract the title by finding the text between the first two periods.
+    title_match = re.search(r'\.\s(.*?)\.\s', citation)
+    
+    # If the title is found, return it.
+    if title_match:
+        return title_match.group(1)
+    
+    # If no title is found, return an error message.
+    return "Title not found"
+
+# Test the function with an example citation.
+citation = "Smith, Thomas, and Barbara Michelle Williams. The Citation Manual for Students: A Quick Guide. Wiley, 2020."
+print(extract_title(citation))
+
+
+
+
+
+
